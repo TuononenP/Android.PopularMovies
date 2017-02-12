4a25b531f073b91a949336a116bbd4b5db5bc9fa
Petri Tuononen <petri.tuononen@gmail.com>
2017-02-12 16:42:10 +0200

    Created MovieDbHelper and MovieProvider.

---
+++ PopularMovies/app/src/main/java/com/petrituononen/popularmovies/data/MovieDbHelper.java
@@ -0,0 +1,44 @@
+package com.petrituononen.popularmovies.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.petrituononen.popularmovies.data.MovieContract.MovieEntry;
+
+/**
+ * Created by Petri Tuononen on 12.2.2017.
+ */
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "movie.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_MOVIE_TABLE =
+
+                "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+                        MovieEntry._ID               + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        MovieEntry.COLUMN_MOVIE_ID       + " INTEGER NOT NULL, "                 +
+                        MovieEntry.COLUMN_TITLE + " STRING NOT NULL,"                  +
+                        MovieEntry.COLUMN_FAVORITE   + " BOOLEAN NOT NULL, "                    +
+                        MovieEntry.COLUMN_POSTER   + " STRING NOT NULL, "                    +
+                        MovieEntry.COLUMN_SYNOPSIS   + " STRING NOT NULL, "                    +
+                        MovieEntry.COLUMN_RATING   + " REAL NOT NULL, "                    +
+                        MovieEntry.COLUMN_RELEASE_DATE + " INTEGER NOT NULL, "                    +
+
+                        " UNIQUE (" + MovieEntry.COLUMN_MOVIE_ID + ") ON CONFLICT REPLACE);";
+
+        db.execSQL(SQL_CREATE_MOVIE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
---
+++ PopularMovies/app/src/main/java/com/petrituononen/popularmovies/data/MovieProvider.java
@@ -0,0 +1,151 @@
+package com.petrituononen.popularmovies.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by Petri Tuononen on 12.2.2017.
+ */
+public class MovieProvider extends ContentProvider {
+
+    public static final int CODE_MOVIE = 100;
+    public static final int CODE_MOVIE_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MovieDbHelper mDbHelper;
+
+    public static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = MovieContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, MovieContract.PATH_MOVIES, CODE_MOVIE);
+
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new MovieDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public int bulkInsert(@NonNull Uri uri, @NonNull ContentValues[] values) {
+        final SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        switch (sUriMatcher.match(uri)) {
+
+            case CODE_MOVIE:
+                db.beginTransaction();
+                int rowsInserted = 0;
+                try {
+                    for (ContentValues value : values) {
+                        long _id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, value);
+                        if (_id != -1) {
+                            rowsInserted++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+
+                if (rowsInserted > 0) {
+                    getContext().getContentResolver().notifyChange(uri, null);
+                }
+
+                return rowsInserted;
+
+            default:
+                return super.bulkInsert(uri, values);
+        }
+    }
+
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        Cursor cursor;
+
+        switch (sUriMatcher.match(uri)) {
+            case CODE_MOVIE:
+                cursor = mDbHelper.getReadableDatabase().query(
+                        MovieContract.MovieEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            case CODE_MOVIE_WITH_ID:
+                String movieId = uri.getLastPathSegment();
+                String[] selectionArguments = new String[]{movieId};
+                cursor = mDbHelper.getReadableDatabase().query(
+                        MovieContract.MovieEntry.TABLE_NAME,
+                        projection,
+                        MovieContract.MovieEntry.COLUMN_MOVIE_ID + " = ? ",
+                        selectionArguments,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        int numRowsDeleted;
+
+        if (null == selection) selection = "1";
+
+        switch (sUriMatcher.match(uri)) {
+
+            case CODE_MOVIE:
+                numRowsDeleted = mDbHelper.getWritableDatabase().delete(
+                        MovieContract.MovieEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs);
+
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (numRowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return numRowsDeleted;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public void shutdown() {
+        mDbHelper.close();
+        super.shutdown();
+    }
+}
